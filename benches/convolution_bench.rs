ca90f9567c9569b6effa90fd1064796c22fbee2f
Shi Nan Li Gao <ligao.shinan@gmail.com>
2025-04-30 12:07:04 +0200

    rework benchmark logic

--- benches/convolution_bench.rs
+++ benches/convolution_bench.rs
@@ -8,11 +8,9 @@ use convolution::{Convolution, Sample};
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use std::f64::consts::PI;
 
-const BLOCK_SIZE: usize = 256;
-const FILTER_LENGTHS: [usize; 14] = [
-    128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576,
-];
-const TRANSITION_SAMPLES: usize = 1048576 + 10 * BLOCK_SIZE;
+const MAX_FILTER_LENGTH: usize = 16384;
+const BLOCK_SIZES: [usize; 3] = [128, 512, 2048];
+const TRANSITION_SAMPLES: usize = 1048576 + 10 * 2048;
 
 pub fn convolver_update_process_benchmarks(c: &mut Criterion) {
     let amplitude = 10.0;
@@ -26,105 +24,124 @@ pub fn convolver_update_process_benchmarks(c: &mut Criterion) {
 
     let mut group = c.benchmark_group("convolver_update_process");
 
-    let input: Vec<Sample> = sinusoid(440.0, amplitude, BLOCK_SIZE);
-    let mut output_setup = vec![0.0; BLOCK_SIZE];
-    let mut output_benchmark = vec![0.0; BLOCK_SIZE];
+    let max_block_size = *BLOCK_SIZES.iter().max().unwrap();
+    let input_long: Vec<Sample> = sinusoid(440.0, amplitude, max_block_size);
 
-    for ir_len in FILTER_LENGTHS.iter() {
-        let response: Vec<Sample> = sinusoid(100.0, 1.0, *ir_len);
+    for block_size in BLOCK_SIZES.iter() {
+        let input = &input_long[0..*block_size];
+        let mut output_setup = vec![0.0; *block_size];
+        let mut output_benchmark = vec![0.0; *block_size];
 
-        group.bench_with_input(
-            BenchmarkId::new("time_domain_crossfade", ir_len),
-            ir_len,
-            |b, _ir_len| {
-                b.iter_batched_ref(
-                    || -> CrossfadeConvolverTimeDomain<FFTConvolverOLS> {
-                        let mut convolver = CrossfadeConvolverTimeDomain::<FFTConvolverOLS>::new(
-                            FFTConvolverOLS::init(&response, BLOCK_SIZE, response.len()),
-                            response.len(),
-                            BLOCK_SIZE,
-                            TRANSITION_SAMPLES,
-                        );
-                        convolver.process(&input, &mut output_setup);
-                        convolver
-                    },
-                    |convolver: &mut CrossfadeConvolverTimeDomain<FFTConvolverOLS>| {
-                        convolver.update(&response);
-                        convolver.process(&input, &mut output_benchmark);
-                    },
-                    criterion::BatchSize::SmallInput,
-                )
-            },
-        );
+        let mut ir_len = 0;
 
-        group.bench_with_input(
-            BenchmarkId::new("frequency_domain_crossfade", ir_len),
-            ir_len,
-            |b, _ir_len| {
-                b.iter_batched_ref(
-                    || -> CrossfadeConvolverFrequencyDomain {
-                        let mut convolver = CrossfadeConvolverFrequencyDomain::init(
-                            &response,
-                            BLOCK_SIZE,
-                            response.len(),
-                        );
-                        convolver.process(&input, &mut output_setup);
-                        convolver
-                    },
-                    {
+        while ir_len < MAX_FILTER_LENGTH {
+            ir_len += block_size;
+            let response: Vec<Sample> = sinusoid(100.0, 1.0, ir_len);
+
+            group.bench_with_input(
+                BenchmarkId::new(
+                    "time_domain_crossfade",
+                    format!("b{}_ir{}", block_size, ir_len),
+                ),
+                &ir_len,
+                |b, _ir_len| {
+                    b.iter_batched_ref(
+                        || -> CrossfadeConvolverTimeDomain<FFTConvolverOLS> {
+                            let mut convolver =
+                                CrossfadeConvolverTimeDomain::<FFTConvolverOLS>::new(
+                                    FFTConvolverOLS::init(&response, *block_size, response.len()),
+                                    response.len(),
+                                    *block_size,
+                                    TRANSITION_SAMPLES,
+                                );
+                            convolver.process(&input, &mut output_setup);
+                            convolver
+                        },
+                        |convolver: &mut CrossfadeConvolverTimeDomain<FFTConvolverOLS>| {
+                            convolver.update(&response);
+                            convolver.process(&input, &mut output_benchmark);
+                        },
+                        criterion::BatchSize::SmallInput,
+                    )
+                },
+            );
+
+            group.bench_with_input(
+                BenchmarkId::new(
+                    "frequency_domain_crossfade",
+                    format!("b{}_ir{}", block_size, ir_len),
+                ),
+                &ir_len,
+                |b, _ir_len| {
+                    b.iter_batched_ref(
+                        || -> CrossfadeConvolverFrequencyDomain {
+                            let mut convolver = CrossfadeConvolverFrequencyDomain::init(
+                                &response,
+                                *block_size,
+                                response.len(),
+                            );
+                            convolver.process(&input, &mut output_setup);
+                            convolver
+                        },
                         |convolver: &mut CrossfadeConvolverFrequencyDomain| {
                             convolver.update(&response);
                             convolver.process(&input, &mut output_benchmark);
-                        }
-                    },
-                    criterion::BatchSize::SmallInput,
-                )
-            },
-        );
+                        },
+                        criterion::BatchSize::SmallInput,
+                    )
+                },
+            );
 
-        group.bench_with_input(
-            BenchmarkId::new("stepwise_update", ir_len),
-            ir_len,
-            |b, _ir_len| {
-                b.iter_batched_ref(
-                    || -> StepwiseUpdateConvolver {
-                        let mut convolver =
-                            StepwiseUpdateConvolver::new(&response, response.len(), BLOCK_SIZE);
-                        convolver.process(&input, &mut output_setup);
-                        convolver
-                    },
-                    |convolver: &mut StepwiseUpdateConvolver| {
-                        convolver.update(&response);
-                        convolver.process(&input, &mut output_benchmark);
-                    },
-                    criterion::BatchSize::SmallInput,
-                )
-            },
-        );
+            group.bench_with_input(
+                BenchmarkId::new("stepwise_update", format!("b{}_ir{}", block_size, ir_len)),
+                &ir_len,
+                |b, _ir_len| {
+                    b.iter_batched_ref(
+                        || -> StepwiseUpdateConvolver {
+                            let mut convolver = StepwiseUpdateConvolver::new(
+                                &response,
+                                response.len(),
+                                *block_size,
+                            );
+                            convolver.process(&input, &mut output_setup);
+                            convolver
+                        },
+                        |convolver: &mut StepwiseUpdateConvolver| {
+                            convolver.update(&response);
+                            convolver.process(&input, &mut output_benchmark);
+                        },
+                        criterion::BatchSize::SmallInput,
+                    )
+                },
+            );
 
-        group.bench_with_input(
-            BenchmarkId::new("faded_stepwise_update", ir_len),
-            ir_len,
-            |b, _ir_len| {
-                b.iter_batched_ref(
-                    || -> FadedStepwiseUpdateConvolver {
-                        let mut convolver = FadedStepwiseUpdateConvolver::new(
-                            &response,
-                            response.len(),
-                            BLOCK_SIZE,
-                            TRANSITION_SAMPLES,
-                        );
-                        convolver.process(&input, &mut output_setup);
-                        convolver
-                    },
-                    |convolver: &mut FadedStepwiseUpdateConvolver| {
-                        convolver.update(&response);
-                        convolver.process(&input, &mut output_benchmark);
-                    },
-                    criterion::BatchSize::SmallInput,
-                )
-            },
-        );
+            group.bench_with_input(
+                BenchmarkId::new(
+                    "faded_stepwise_update",
+                    format!("b{}_ir{}", block_size, ir_len),
+                ),
+                &ir_len,
+                |b, _ir_len| {
+                    b.iter_batched_ref(
+                        || -> FadedStepwiseUpdateConvolver {
+                            let mut convolver = FadedStepwiseUpdateConvolver::new(
+                                &response,
+                                response.len(),
+                                *block_size,
+                                TRANSITION_SAMPLES,
+                            );
+                            convolver.process(&input, &mut output_setup);
+                            convolver
+                        },
+                        |convolver: &mut FadedStepwiseUpdateConvolver| {
+                            convolver.update(&response);
+                            convolver.process(&input, &mut output_benchmark);
+                        },
+                        criterion::BatchSize::SmallInput,
+                    )
+                },
+            );
+        }
     }
     group.finish();
 }
